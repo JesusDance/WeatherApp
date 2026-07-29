a8e2b5f0f4a9341929d952319586024a57d8878f
Jesus <jiesusdance@gmail.com>
2026-07-29 16:31:11 +0300

    added redis dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,7 +2,10 @@ from typing import Annotated
 
 from fastapi.params import Depends
 from httpx import AsyncClient
+from redis.asyncio import Redis
 
+from app.cache.redis_client import get_redis_client
 from app.client import get_client
 
-CLIENT = Annotated[AsyncClient, Depends(get_client)]
+CLIENT_DEP = Annotated[AsyncClient, Depends(get_client)]
+REDIS_DEP = Annotated[Redis, Depends(get_redis_client)]
