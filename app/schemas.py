013c2e519f44c292bdef949aea77fad7f2639f34
Jesus <jiesusdance@gmail.com>
2026-07-29 16:33:20 +0300

    added schema for forecast

--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel, Field
 
 
@@ -9,3 +11,15 @@ class ReadWeatherData(BaseModel):
     city: str
     temp: float
     wind: float
+
+
+class ReadDay(BaseModel):
+    temp_min: float
+    temp_max: float
+    feels_like: float
+    dt_txt: datetime
+
+
+class ReadWeatherFiveDays(BaseModel):
+    city: str
+    days: list[ReadDay]
