ca7d11ec76d25ce0b36f962afbbf538eacd87d2a
Jesus <jiesusdance@gmail.com>
2026-07-29 16:30:23 +0300

    added redis variables

--- app/config.py
+++ app/config.py
@@ -5,7 +5,11 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     URL: str = f"https://api.openweathermap.org/data/2.5/weather"
+    FORECAST_URL: str = f"https://api.openweathermap.org/data/2.5/forecast"
     API_KEY: str
-
+    REDIS_URL: str = "redis://localhost:6379/0"
+    REQUESTS_LIMIT: int = 10
+    CACHE_EXPIRE_SECONDS: int = 60
+    CACHE_TTL_SECONDS: int = 600
 
 settings = Settings()
